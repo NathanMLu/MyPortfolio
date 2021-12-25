de3cc4e772fc5bb847f7fac665278fa516ede5b3
Nathan Lu <nathanlumax@gmail.com>
2021-12-24 22:51:49 -0600

    dependency

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three/build/three.module.js';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 let scene, camera, renderer, loader;
